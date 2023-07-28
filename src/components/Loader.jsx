d14b26b3f55735fb69ffd4bb916ce1b69e3ca2ea
김우빈 <kwb020312@naver.com>
2023-07-29 01:14:54 +0900

    Feat: 모바일 UI 적용 및 로딩 gif 생성

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import AnimationLogo from "../assets/images/logo-animated.gif";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={AnimationLogo} alt="Apple loader" className="logo" />
+    </div>
+  );
+};
+
+export default Loader;
